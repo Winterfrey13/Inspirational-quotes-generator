a5c885fd39013efa71dac74f38456dc5cc0b113e
Winterfrey13 <oumououangre@gmail.com>
2024-02-12 00:11:27 -0500

    improved the user experience

--- src/index.js
+++ src/index.js
@@ -16,9 +16,13 @@ function generateQuote(event) {
   let apiKey = "0028ba044ca06fc2b3teda9ee7f81ob0";
   let prompt = `User's instructions: Generate two inspirational quotes about ${instructionsInput.value}`;
   let context =
-    "You are a skilled quote expert and love to write inspirational quotes. Your mission is to generate two random remarquable inspirational quote in basic HTML. Make sure to follow the user's instructions. Do not include a title to the quote. Sign the quote with `SheCodesAI` only in a <br />, <small>, <em> and <strong> element";
+    "You are a skilled quote expert and love to write inspirational quotes. Your mission is to generate two random remarquable inspirational quote in basic HTML. Make sure to follow the user's instructions. Do not include a title to the quote. Sign the quote with `SheCodesAI` ONLY in a <br />, <small>, <em> and <strong> element";
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = ` <div class= "generating"> ⏳ Generating an inspirational quote about ${instructionsInput.value} </div>`;
 
   console.log("Generating quote...");
   console.log(`prompt: ${prompt}`);
